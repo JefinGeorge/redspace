2b6fd7c7d6dc3f2e3c44871eae5a6b38b09b1df7
Jeffin George Jose <jeffin101@gmail.com>
2021-02-25 23:09:21 -0400

    New Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
 import { MatSelectModule } from '@angular/material/select';
+import { InfoComponent } from './info/info.component';
+import { PlanetComponent } from './planet/planet.component';
+import { SpeciesComponent } from './species/species.component';
+import { FilmsComponent } from './films/films.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    PlanetComponent,
+    SpeciesComponent,
+    FilmsComponent
   ],
   imports: [
     BrowserModule,
